d0f19ffe5191d71d82d6ef94827ac48ac13ae86a
mathieutranchida <mathieu.tranchida@gmail.com>
2020-11-09 15:38:23 -0500

    fetch 1 done

---
+++ client/src/components/CurrentUserContext.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+export const CurrentUserContext = React.createContext(null);
+
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = React.useState(null);
+  const [status, setStatus] = React.useState("loading");
+
+  useEffect(() => {
+    fetch(`/api/me/profile`)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        setCurrentUser(data.profile);
+        setStatus("idle");
+      });
+  }, []);
+
+  return (
+    <CurrentUserContext.Provider
+      value={{
+        currentUser,
+        status,
+      }}
+    >
+      {children}
+    </CurrentUserContext.Provider>
+  );
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { CurrentUserProvider } from "./components/CurrentUserContext";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <CurrentUserProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </CurrentUserProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
